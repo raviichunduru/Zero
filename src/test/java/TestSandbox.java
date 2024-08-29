fa443142be239dcf1c0b8870856eb8b0f5bffbb7
Raghavaiah Chunduru <145857386+raviichunduru@users.noreply.github.com>
2024-08-29 21:12:57 +0530

    Added sandbox test (#5)
    * test if direct push to main is restricted or not
    
    * added TestSandbox.java
    
    * test if direct push to main is restricted or not

--- src/test/java/TestSandbox.java
+++ src/test/java/TestSandbox.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -8,6 +7,16 @@ public class TestSandbox {
    * a very basic test
    */
   @Test
+  void assertThatTrueIsTrue() throws InterruptedException {
+    assertTrue(true, "true is true");
+    Thread.sleep(2000);
+  }
+
+  @Test
+  void assertThatDayIsADay() throws InterruptedException {
+    assertEquals("day", "day","day is a day");
+    Thread.sleep(2000);
+    
   void assertThatTrueIsTrue() {
     assertTrue(true, "true is true");
   }

5e9b291c01b55dbcbf66b4b06c80972a6603b2e2
admin <ravii.chunduru@gmail.com>
2024-08-31 17:48:00 +0530

    added custom annotations & reusable workflows

---
+++ src/main/java/annotations/FailingTest.java
@@ -0,0 +1,15 @@
+package annotations;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("failing")
+@Test
+public @interface FailingTest {
+}

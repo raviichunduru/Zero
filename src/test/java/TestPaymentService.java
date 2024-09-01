b8a5ba13fc763a3c66dea3bdf804408d5d7a2823
Raghavaiah Chunduru <145857386+raviichunduru@users.noreply.github.com>
2024-09-01 13:36:59 +0530

    Trigger tests on external event (#36)
    * test if direct push to main is restricted or not
    
    * added log4j.properties
    
    * added custom annotations & reusable workflows
    
    * added custom annotations & reusable workflows
    
    * added custom annotations & reusable workflows
    
    * added custom annotations & reusable workflows
    
    * added custom annotations & reusable workflows
    
    * added custom annotations & reusable workflows
    
    * refactoring reusable work flows
    
    * refactoring reusable work flows
    
    * refactoring reusable work flows
    
    * refactoring reusable work flows
    
    * added TestEmployeeService.java, TestPaymentService.java and trigger-tests-on-external-event.yml

---
+++ src/test/java/TestPaymentService.java
@@ -0,0 +1,25 @@
+import annotations.SmokeTest;
+import com.typesafe.config.Config;
+import config.TestEnvFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Tag;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("payment-service")
+@Slf4j
+public class TestPaymentService {
+
+  @SmokeTest
+  void assertThatTrueIsTrue() {
+    assertTrue(true, "true is true");
+  }
+
+  @SmokeTest
+  void assertThatTestForChosenEnvRuns() {
+    Config CONFIG = TestEnvFactory.getInstance().getConfig();
+    String expectedEnv = CONFIG.getString("TEST_ENV");
+    log.info("expectedEnv is : {}",expectedEnv);
+    assertEquals(expectedEnv, CONFIG.getString("TEST_ENV"),"TEST_ENV");
+  }
+}

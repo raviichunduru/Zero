757ed9fbcdac2c519380388ae85f69f6a3b2acf8
admin <ravii.chunduru@gmail.com>
2024-09-05 21:10:32 +0530

    added config switch to send results to ElastiscSearch or not

--- src/main/java/extensions/ReportingExtension.java
+++ src/main/java/extensions/ReportingExtension.java
@@ -1,14 +1,27 @@
 package extensions;
 
+import com.typesafe.config.Config;
+import config.TestEnvFactory;
 import extensions.report.PublishResults;
 import extensions.report.TestRunMetaData;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
+@Slf4j
 public class ReportingExtension implements AfterEachCallback {
+
+  private static final Config CONFIG = TestEnvFactory.getInstance().getConfig();
+  private static final Boolean PUBLISH_RESULTS_TO_ELASTIC = CONFIG.getBoolean("PUBLISH_RESULTS_TO_ELASTIC");
+
   @Override
   public void afterEach(ExtensionContext context) throws Exception {
-    TestRunMetaData testRunMetaData = new TestRunMetaData().setBody(context);
-    PublishResults.toElastic(testRunMetaData);
+    if (PUBLISH_RESULTS_TO_ELASTIC) {
+      log.info("publishing results to elastic");
+      TestRunMetaData testRunMetaData = new TestRunMetaData().setBody(context);
+      PublishResults.toElastic(testRunMetaData);
+    } else {
+      log.info("Skipping publishing results to console since PUBLISH_RESULTS_TO_ELASTIC=false");
+    }
   }
 }
